fdcec4983add5757cc709cab36966699094cc1cb
Nazim-Yoldas Sesen <nazimsesen@gmail.com>
2021-06-19 15:10:09 -0400

    Added Hero aspects

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,17 +3,17 @@ import React from 'react';
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
 import { DiCssdeck } from 'react-icons/di';
 
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
+import { Container, Div1, Div2, Div3, NavLink, SocialIcons, Span } from './HeaderStyles';
 
 const Header = () =>  (
   <Container>
     <Div1>
       <Link href="/">
-        <a style={{ display: "flex", alignItems: "center", color: 'white'}}>
+        <a style={{ display: "flex", alignItems: "center", color: 'white', marginBottom:"20px"}}>
           <DiCssdeck size="3rem"/>
-          <span>
+          <Span>
             Portolio
-          </span>
+          </Span>
         </a>
       </Link>
     </Div1>
@@ -40,6 +40,17 @@ const Header = () =>  (
         </Link>
       </li>
     </Div2>
+    <Div3>
+      <SocialIcons href="https://github.com">
+        <AiFillGithub size="3rem" />
+      </SocialIcons>
+      <SocialIcons href="https://facebook.com">
+        <AiFillInstagram size="3rem" />
+      </SocialIcons>
+      <SocialIcons href="https://linkedin.com">
+        <AiFillLinkedin size="3rem" />
+      </SocialIcons>
+    </Div3>
   </Container>
 );
 
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,18 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome to <br />
+        My Practise Portfolio
+      </SectionTitle>
+      <SectionText>
+        This is a practise portfolio that I created in order to better my training in the latest technologies used in webtech companies across the globe.
+      </SectionText>
+      <Button onClick={() => window.location = 'https://google.com'}>Learn More!</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
